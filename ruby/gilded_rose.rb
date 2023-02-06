8b708d8ba12002b0841e0722fa7e1bab30286139
Jude Etanuwoma <jude.etanuwoma@andela.com>
2023-02-06 11:20:49 +0000

    Refactor: move items to classes

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -4,52 +4,8 @@ class GildedRose
     @items = items
   end
 
-  def update_quality()
-    @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-      end
-    end
+  def update_quality
+    @items.each { |item| ItemService.update(item) }
   end
 end
 
@@ -66,3 +22,61 @@ class Item
     "#{@name}, #{@sell_in}, #{@quality}"
   end
 end
+
+class ItemService
+  attr_accessor :item
+
+  def initialize(item)
+    @item = item
+  end
+
+  def self.update(item)
+    case item.name
+    when 'Backstage passes to a TAFKAL80ETC concert'
+      Backstage.new(item).update
+    when 'Aged Brie'
+      Brie.new(item).update
+    when 'Sulfuras, Hand of Ragnaros'
+    else
+      # raise not supported
+    end
+  end
+
+  def update
+    update_sell_in
+    update_quality
+  end
+
+  def update_sell_in
+    item.sell_in = item.sell_in - 1
+  end
+
+  def update_quality; end
+end
+
+class Backstage < ItemService
+  def update_quality
+    return item.quantity = 0 if item.sell_in >= 49
+
+    item.quality += \
+      case item.sell_in
+      when 0...6
+        3
+      when 6...10
+        2
+      when 10..
+        1
+      else
+        0
+      end
+  end
+end
+
+class Brie < ItemService
+  def update_quality
+    item.quality = item.quality + 1 if item.quality < 50 && (item.sell_in <= 0)
+    item.quality = item.quality + 1 unless item.quality >= 50
+  end
+end
+
+class Sulfuras < ItemService; end
--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -2,12 +2,119 @@ require File.join(File.dirname(__FILE__), 'gilded_rose')
 
 describe GildedRose do
 
-  describe "#update_quality" do
-    it "does not change the name" do
-      items = [Item.new("foo", 0, 0)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "fixme"
+  describe '#update_quality' do
+
+    describe 'Backstage items' do
+      context 'backstage sell_in minus_one' do
+        let(:items) { [Item.new('Backstage passes to a TAFKAL80ETC concert', -1, 0)] }
+
+        it 'should update item with right value' do
+          GildedRose.new(items).update_quality
+          expect(items[0].sell_in).to be(-2)
+          expect(items[0].quality).to be(0)
+        end
+      end
+
+      context 'backstage sell_in 10' do
+        let(:items) { [Item.new('Backstage passes to a TAFKAL80ETC concert', 10, 0)] }
+
+        it 'should update item with right value' do
+          GildedRose.new(items).update_quality
+          expect(items[0].sell_in).to be(9)
+          expect(items[0].quality).to be(2)
+        end
+      end
+
+      context 'backstage sell_in 0' do
+        let(:items) { [Item.new('Backstage passes to a TAFKAL80ETC concert', 0, 0)] }
+
+        it 'should update item with right value' do
+          GildedRose.new(items).update_quality
+          expect(items[0].sell_in).to be(-1)
+          expect(items[0].quality).to be(0)
+        end
+      end
+
+      context 'backstage quality is 49 and above' do
+        let(:items) { [Item.new('Backstage passes to a TAFKAL80ETC concert', 0, 0)] }
+
+        it 'should update item with right value' do
+          GildedRose.new(items).update_quality
+          expect(items[0].sell_in).to be(-1)
+          expect(items[0].quality).to be(0)
+        end
+      end
+    end
+
+    describe 'Brie items' do
+      context 'Brie sell_in minus_one' do
+        let(:items) { [Item.new('Aged Brie', 11, 0)] }
+
+        it 'should update item with right value' do
+          GildedRose.new(items).update_quality
+          expect(items[0].sell_in).to be(10)
+          expect(items[0].quality).to be(1)
+        end
+      end
+
+      context 'Brie sell_in 0' do
+        let(:items) { [Item.new('Aged Brie', 0, 0)] }
+
+        it 'should update item with right value' do
+          GildedRose.new(items).update_quality
+          expect(items[0].sell_in).to be(-1)
+          expect(items[0].quality).to be(2)
+        end
+      end
+
+      context 'brie quality is 49 and above' do
+        let(:items) { [Item.new('Aged Brie', 0, 50)] }
+
+        it 'should update item with right value' do
+          GildedRose.new(items).update_quality
+          expect(items[0].sell_in).to be(-1)
+          expect(items[0].quality).to be(50)
+        end
+      end
+    end
+
+    describe 'Sulfuras items' do
+      context 'sulfuras sell_in minus_one' do
+        let(:items) { [Item.new('Sulfuras, Hand of Ragnaros', -1, 0)] }
+
+        it 'should update item with right value' do
+          GildedRose.new(items).update_quality
+          expect(items[0].sell_in).to be(-1)
+          expect(items[0].quality).to be(0)
+        end
+      end
+
+
+      context 'when sulfuras sell_in is 10' do
+        let(:items) { [Item.new('Sulfuras, Hand of Ragnaros', 10, 0)] }
+
+        it 'should update item with right value' do
+          GildedRose.new(items).update_quality
+          expect(items[0].sell_in).to be(10)
+          expect(items[0].quality).to be(0)
+        end
+      end
+
+      context 'sulfuras quality is 50' do
+        let(:items) { [Item.new('Sulfuras, Hand of Ragnaros', 0, 50)] }
+
+        it 'should update item with right value' do
+          GildedRose.new(items).update_quality
+          expect(items[0].sell_in).to be(0)
+          expect(items[0].quality).to be(50)
+        end
+      end
+    end
+
+    it 'does not change the name' do
+      items = [Item.new('foo', 0, 0)]
+      GildedRose.new(items).update_quality
+      expect(items[0].name).to eq 'foo'
     end
   end
-
 end
